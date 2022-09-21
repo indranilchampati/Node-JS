346688b701b1c0562852b5aa759885120ec98191
Indranil Champati <63838235+indranilchampati@users.noreply.github.com>
2022-09-21 10:47:10 +0530

    HOL -- 6

--- Day 6/app.js
+++ Day 6/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const port = 3000;
-
+//
 app.use(express.static("./public")); //before this CSS was not working and page was just showing things but not working
 
 app.get("/", function (req, res) {
